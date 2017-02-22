64013ec304977ebc71aed42dee7b049fde64aa01
marcopete5 <mdabomb5@gmail.com>
2017-02-21 17:16:53 -0700

    adding directives

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
-var app = angular.module('techApp');
+var app = angular.module('techApp', []);
+
+app.controller('mainController', ['$scope', '$http', function ($scope, $http){
+
+	$scope.warren = {
+		name: 'Warren Buffett',
+		email: 'email@email.com'
+	};
+
+}])
+
+app.directive('wishlist', function() {
+	return {
+		scope: {
+			'contactInfo': '=wishlist'
+		},
+		template: "<span>{{contactInfo.name}}</span>",
+		restrict: 'A'
+	}
+})
+
 
